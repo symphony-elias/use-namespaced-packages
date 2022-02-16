e823c5232be342b9a4090314706a41141e03e1f1
Elias Croze <elias.croze@symphony.com>
2022-02-16 09:47:12 +0100

    Using BDK with namespace packages

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from eliasc_pkg import a
 from eliasc_pkg import b
+from symphony.bdk.core.symphony_bdk import SymphonyBdk
+from symphony.bdk.extension import ExtensionService
 
-
-def print_hi(name):
+if __name__ == '__main__':
     a.a_function()
     b.b_function()
 
+    service = ExtensionService()
+    service.register(ValueError())
+    print(service._extensions)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    SymphonyBdk()
